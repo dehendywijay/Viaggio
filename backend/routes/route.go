12b9ee174e0e57b48816521f40f2af6387e55277
dehendyw <dehendywijaya2@gmail.com>
2026-05-27 14:27:45 +0700

    refactor: membuat sesuai clean arsitecture

--- backend/routes/route.go
+++ backend/routes/route.go
@@ -1,52 +1,33 @@
 package routes
 
 import (
-	controller "triptix/internal/controllers"
-	"triptix/internal/repository"
-	"triptix/internal/services"
+	"triptix/config"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
-var db *gorm.DB
+func SetupRoutes(r *gin.Engine, app *config.App) {
 
-func WisataRoute(r *gin.Engine) {
-	wisata := r.Group("/api/wisata")
-	{
-		wisata.POST("", controller.CreateWisata)
-		wisata.GET("", controller.GetAllWisata)
-		wisata.GET("/:id", controller.GetWisataByID)
-		wisata.PUT("/:id", controller.EditWisata)
-	}
-
-}
-
-func UserRoute(r *gin.Engine) {
-	authRepo := &repository.AuthRepository{
-		GormDB: db,
-	}
-	authService := services.NewAuthService(authRepo)
-	authController := controller.NewAuthControllers(authService)
 	user := r.Group("/api/user")
 	{
-		user.POST("/register", authController.RegisterUser)
-		user.POST("/login", authController.LoginUser)
-		user.GET("/:email", authController.GetUser)
-		user.POST("/logout", authController.LogoutUser)
-		user.POST("/refresh", authController.RefreshToken)
+		user.POST("/register", app.AuthController.RegisterUser)
+		user.POST("/login", app.AuthController.LoginUser)
+		user.GET("/:email", app.AuthController.GetUser)
+		user.POST("/logout", app.AuthController.LogoutUser)
+		user.POST("/refresh", app.AuthController.RefreshToken)
 	}
-}
 
-func ReviewRoute(r *gin.Engine) {
-	reviewRepo := &repository.ReviewRepository{
-		GormDB: db,
-	}
-	reviewService := services.NewReviewService(reviewRepo)
-	reviewController := controller.NewReviewControllers(reviewService)
 	review := r.Group("/api/reviews")
 	{
-		review.POST("", reviewController.CreateReview)
-		review.GET("/wisata/:wisata_id", reviewController.GetReviewsByWisata)
+		review.POST("", app.ReviewController.CreateReview)
+		review.GET("/wisata/:wisata_id", app.ReviewController.GetReviewsByWisata)
+	}
+
+	wisata := r.Group("/api/wisata")
+	{
+		wisata.POST("", app.WisataController.CreateWisata)
+		wisata.GET("", app.WisataController.GetAllWisata)
+		wisata.GET("/:id", app.WisataController.GetWisataByID)
+		wisata.PUT("/:id", app.WisataController.EditWisata)
 	}
 }
