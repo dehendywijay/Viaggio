321e8710d5f558fddacaed138b54d3b2618c0147
rireeee666 <ridhoresky436@gmail.com>
2026-06-07 19:26:31 +0700

    feat: add API constants for user, wisata, reviews, and orders

---
+++ cms/constans/strings.ts
@@ -0,0 +1,12 @@
+export const api = 'http://localhost:8080/api';
+
+export const api_wisata = `${api}/wisata`;
+
+export const api_user = `${api}/user`;
+export const api_user_login = `${api_user}/login`;
+export const api_user_logout = `${api_user}/logout`;
+export const api_user_refresh = `${api_user}/refresh`;
+
+export const api_reviews = `${api}/reviews`;
+
+export const api_orders = `${api}/orders`;
